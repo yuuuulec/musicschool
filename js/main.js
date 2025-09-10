1357a60ccdae9b97b0eaa9f721e05e63f9d4e892
’yuuulec’ <sprouse.yuuka@gmail.com>
2025-09-11 04:50:42 +0900

    top

---
+++ js/main.js
@@ -0,0 +1,88 @@
+//お問い合わせ　トップに戻るボタン　スクロール
+$(function() {
+    const headerHeight = $('#header').outerHeight(); 
+    const fv = $('.fv'); 
+    let fvHeight = fv.outerHeight();
+
+    $(window).on('load resize', function() {
+        fvHeight = fv.outerHeight();
+    });
+
+    const showOffset = headerHeight + fvHeight;
+
+    $(window).on('scroll', function() {
+        const scroll = $(window).scrollTop();
+        if (scroll > showOffset) {
+            $('.c-to-top, .c-contact__btn').fadeIn();
+        } else {
+            $('.c-to-top, .c-contact__btn').fadeOut();
+        }
+    });
+
+    // 初期非表示
+    $('.c-to-top, .c-contact__btn').hide();
+
+    // トップへスムーズスクロール
+    $('.c-to-top a').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, 600);
+    });
+});
+
+
+
+
+
+//アコーディオン
+$(function () {
+    $(".js-faq-toggle").attr("aria-expanded", "false");
+    $(".faq__answer").hide();
+
+    function toggleFaq($toggle, $answer) {
+        const isOpen = $answer.is(":visible");
+        if (!isOpen) {
+            $answer.css("display", "flex").hide().slideDown(400);
+            $toggle.attr("aria-expanded", true);
+        } else {
+            $answer.slideUp(400);
+            $toggle.attr("aria-expanded", false);
+        }
+    }
+
+    $(".js-faq-toggle").on("click", function () {
+        const $this = $(this);
+        const $answer = $this.next(".faq__answer");
+        toggleFaq($this, $answer);
+    });
+
+    $(".faq__answer").on("click", function () {
+        const $answer = $(this);
+        const $toggle = $answer.prev(".js-faq-toggle");
+        toggleFaq($toggle, $answer);
+    });
+});
+
+
+//生徒さんたちの声　スライダー
+const voiceSwiper = new Swiper('.voice__inner', {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: false,
+    navigation: {
+        nextEl: '.voice__next',
+        prevEl: '.voice__prev',
+    },
+    breakpoints: {
+        0: { slidesPerView: 1 },
+        768: { slidesPerView: 2 },
+        1024: { slidesPerView: 3 }
+    },
+    observer: true,        
+    observeParents: true,
+});
+
+initVoiceSwiper();
+
+window.addEventListener('resize', () => {
+    initVoiceSwiper(); // リサイズ時に再初期化
+});
